6fe6a22fd3d3cdad2336c97d1040d6d7f8aaba14
Nand <nknittin16436@gmail.com>
2023-02-24 17:14:39 +0530

    Removed emnu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,10 @@
-import React, { useState } from 'react';
-
 import './App.css';
 import HomePage from './components/Home/HomePage';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
-const items: MenuProps['items'] = [
-  {
-    label: 'Navigation One',
-    key: 'mail',
-  },
-  {
-    label: 'Navigation Two',
-    key: 'app',
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-    key: 'alipay',
-  },
-];
+
 function App() {
-  const [current, setCurrent] = useState('mail');
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
-    setCurrent(e.key);
-  };
+
   return (
     <div className="App">
-      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
       <HomePage />
     </div>
   );
